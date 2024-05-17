b54813f2c91a8c304f83f72b4658ceaf4f16936a
Ava Hewitt <ahewitt10135@gmail.com>
2024-05-17 13:56:31 -0400

    Day one

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hello, Enter a CC Number");
+        System.out.println(Credit.findDigits(input.nextLong()) + " digits");
     }
-}
+
+
+
+}//last ending
