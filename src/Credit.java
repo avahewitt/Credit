30340e1375e2ebd965f0639bcee33935e0b1bf5a
avahewitt <ahewitt10135@gmail.com>
2024-05-17 14:02:30 -0400

    Update Credit.java
    Added get digit method

--- src/Credit.java
+++ src/Credit.java
@@ -1,13 +1,31 @@
 
 public class Credit {
+    
     public static int findDigits(long num){
         int count = 0;
         while (num > 0){
             int digit = (int)num % 10;
             count++;
             num = num / 10;
-
         }
         return count;
-    }
-}
+    }//ending findDigits
+
+    public static int getDigits(long num, int index){
+        int result = 0;
+        while (num > 0){
+            int digit = (int)num % 10;
+            count++;
+            if(count == index){
+                return digit;
+            }
+            num = num / 10;
+        }
+        return result;
+    }//ending getDigits
+    
+
+
+
+    
+}//last closing
